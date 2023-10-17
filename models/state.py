c5c84377a1ba7f0d2a625a40e8d67531ac5d50fd
kachinne <nobbnet@gmail.com>
2023-10-17 09:24:21 -0700

    Making it walk

---
+++ models/state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defining the state class."""
+from model.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Represents a state.
+
+
+    Attributes:
+    name (str): The name of the state.
+    """
+
+    name = ""
